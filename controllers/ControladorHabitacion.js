9249f86a9d95e1956e7677296ce8115dfba65f17
201 <201@academicos.cesde.edu.co>
2022-08-23 20:31:47 -0500

    Se crea proyecto

---
+++ controllers/ControladorHabitacion.js
@@ -0,0 +1,138 @@
+//en la carpeta de controladores se ejecuta la logica de negocio (el controlador recibe la peticion(que viene de rutas) y la responde)
+
+import {ServicioHabitacion} from '../servicios/servicioHabitacion.js'
+//En el archivo de controllers se configuran las respuestas 
+//uso export para poder usar el controlador y poderlo llamar
+ export class ControladorHabitacion {
+
+    //dentro de mi clase voy hacer metodos, cada metodo estara asociado a cada uno de las rutas o peticiones que voy atender (get-post.etc)
+
+    constructor(){}
+
+    //buscar habitaciones
+    buscarHabitaciones(request,response){
+        //llamo a un servicio, para llamar al servicio debo usar una clase
+
+        let servicioHabitacion = new ServicioHabitacion
+        // siempre un controlador va intentar dar solucion a la Peticion, pero es posible que no lo logre por eso hay que programar un catch
+        try{
+            
+            response.status(200).json({
+                mensaje:"Exito en la consulta",
+                datos: servicioHabitacion.buscarTodas()
+            })
+
+        } catch(error){
+            //cuando se va por el catch  se fallo resolviendo la peticion
+
+            response.status(400).json({
+                mensaje:"Fallo en la consulta" + error,
+                datos:null
+            })
+        }
+    }
+ 
+
+    //buscar habitacion por ID
+    buscarHabitacionPporId(request,response){
+        ///recibi un parametro que se llama id que viene por la peticion y lo almacene en una variable que se llama identificador
+        
+    
+        let identificador = request.params.id
+
+        //llamo al servicio
+        let servicioHabitacion = new ServicioHabitacion
+       
+        try{
+            
+            response.status(200).json({
+                mensaje:"Exito en la consulta " + identificador,
+                datos: servicioHabitacion.buscarPorId()
+            })
+
+        } catch(error){
+       
+
+            response.status(400).json({
+                mensaje:"Fallo en la consulta" + error,
+                datos:null
+            })
+        }
+    }
+ 
+        
+    
+
+
+    //agregar habitacion
+    agregarHabitacion(request,response){
+        
+        let cuerpo=Request.body
+
+          //llamo al servicio
+          let servicioHabitacion = new ServicioHabitacion
+       
+        try{
+            servicioHabitacion.agregar(cuerpo)
+            response.status(200).json({
+                mensaje:"Exito agregando la habitacion",
+                datos: null
+            })
+
+        } catch(error){
+       
+
+            response.status(400).json({
+                mensaje:"Fallo en la consulta" + error,
+                datos:null
+            })
+        }
+    }
+ 
+    
+
+//Lo primero que hace un controlador es esculcar la peticion
+    //Editar habitacion
+    editarHabitacion(request,response){
+
+        //recibir el id como parametro
+        let id=request.params.id
+
+        //recibir los datos que voy a editar(body)
+        let datos= request.body
+
+         //llamo al servicio
+         let servicioHabitacion = new ServicioHabitacion
+       
+        
+        //El controlador responde, le responde al cliente
+        try{
+            servicioHabitacion.actualizar(id,datos)
+            response.status(200).json({
+                mensaje:"Exito editando habitacion" +id,
+                datos:null
+            })
+
+        } catch(error){
+            
+
+            response.status(400).json({})
+        }
+    }
+ 
+    
+
+
+    //eliminar habitacion
+    elimarHabitacion(request,response){
+        try{
+            
+            response.status(200).json({})
+
+        } catch(error){
+            
+            response.status(400).json({})
+        }
+    }
+ 
+}
---
+++ servidor/Servidor.js
@@ -0,0 +1,57 @@
+//llamamos a express ven estas dos lineas
+//const express = require('express') vieja forma de importar
+
+import express from'express' //nueva forma para traer express 
+
+// llamamos las rutas
+import {rutas} from '../routes/rutas.js'
+
+//llamar al metodo conectar con BD - del archivo de conexcion
+import {conectar} from '../dataBase/conexion.js'
+//creo una clase llamada Servidor
+// VOY A EXPORTAR LA CLASE PARA QUE SE PUEDA VER EN OTROS ARCHIVOS
+ export class Servidor{
+    //Dentro de una clase van atributos(Variables) y metodos(funciones) y se definen dentro del constructor
+
+
+    constructor(){
+        this.app = express() // definiendo un atributo(variable), pero como es un atributo, ya no se llama const app como se programo en app.js, si no this.app
+        //clase conjunto de variables(atributos) y funciones(metodos) que estan desribiendo algo
+        this.habilitarBody()
+        this.atenderPeticiones() //atiendo las peticiones del usuario
+     
+    }
+
+    atenderPeticiones(){ //estamos enrutando peticiones
+        //los servicios son operaciones en BD, que son de 4 tipos: GET,POST,PUT,DELETE  (con nombres end point)
+        //recibe peticiones de tipo get(end point,funcion anonima(se ejecuta en el mismo momento))
+       // this.app.get('/' //function (req, res) {
+            //aca responde y envia(recibe y procesa)
+         // res.send('Hello World') COPIO Y PEGO LA FUNCION EN ROUTES
+        //)
+        //cambio el this.app.get por this.app.use=express
+      
+        this.app.use('/',rutas)// atributo una variable
+     
+    }
+
+    habilitarBody(){
+        this.app.use(express.json()) //habilitar(permitiendo) los datos que vengan en json sean leidos
+    }
+
+    encenderServidor(){
+        //PROCESS.ENV.PORT ES PARA ACCEDER AL PUERTO DE LA VARIABLE DE ENTORNO DEL SISTEMA DONDE YO DESPLIEGO O EL DEL ARCHIVO .ENV
+        
+        this.app.listen(process.env.PORT,function(){
+            // colback:  siempre se va a ejecutar primero lo que hay antes de la coma y luego la funcion
+            console.log("servidor encendido "+ process.env.PORT)
+        })
+                
+    }
+
+    conectarConBd(){
+        conectar()
+    }
+}
+
+//  carpeta de routes me permite separar todos los servicios que yo voy a tener
